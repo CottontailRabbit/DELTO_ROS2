7fadc16809ede536a51e7283f7352209abaf41b2
hongcheol <99775161+CottontailRabbit@users.noreply.github.com>
2024-09-13 15:05:20 +0900

    Update delto_modbus_TCP.py

--- delto_3f_driver/delto_utility/delto_modbus_TCP.py
+++ delto_3f_driver/delto_utility/delto_modbus_TCP.py
@@ -57,7 +57,7 @@ class Communication:
                                          sys._getframe().f_code.co_name)            
             return
         
-        self.client.close()  
+        #self.client.close()  
 
     def get_position(self):
         
